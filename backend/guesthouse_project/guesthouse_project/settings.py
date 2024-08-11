68ed3b67d482afd446f5cc1e201d4d78129cbec9
Welf9i <welfkimedamine@gmail.com>
2024-08-12 00:53:40 +0100

    add database url

--- backend/guesthouse_project/guesthouse_project/settings.py
+++ backend/guesthouse_project/guesthouse_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,15 +92,21 @@ WSGI_APPLICATION = 'guesthouse_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'guesthouse_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'aaaa',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'guesthouse_db',
-        'USER': 'postgres',
-        'PASSWORD': 'aaaa',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
