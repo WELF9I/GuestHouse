2b197c9e43f6c1cc1e754a817b6321d4b7ac635e
Welf9i <welfkimedamine@gmail.com>
2024-08-12 20:24:45 +0100

    update settings.py

--- backend/guesthouse_project/guesthouse_project/settings.py
+++ backend/guesthouse_project/guesthouse_project/settings.py
@@ -54,12 +54,12 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
@@ -152,5 +152,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True  # For development only. In production, specify the allowed origins.
+# CORS_ALLOW_ALL_ORIGINS = True  # For development only. In production, specify the allowed origins.
+CORS_ALLOWED_ORIGINS=[
+    'https://guest-house-fawn.vercel.app/',
+]
 CORS_ALLOW_CREDENTIALS = True
