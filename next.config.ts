eafc8cb366bdf958af1c030a9c19e0874fd1c1e6
Welf9i <welfkimedamine@gmail.com>
2024-08-12 02:01:07 +0100

    update next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+}
+
+export default nextConfig
