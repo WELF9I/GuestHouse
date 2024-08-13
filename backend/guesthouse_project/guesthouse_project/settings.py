1326713f8a36b61ee4c7625dc81a62041fedd109
Welf9i <welfkimedamine@gmail.com>
2024-08-13 02:58:06 +0100

    update settings

--- backend/guesthouse_project/guesthouse_project/settings.py
+++ backend/guesthouse_project/guesthouse_project/settings.py
@@ -147,7 +147,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# CORS_ALLOW_ALL_ORIGINS = True  # For development only. In production, specify the allowed origins.
+CORS_ALLOW_ALL_ORIGINS = True  # For development only. In production, specify the allowed origins.
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     'https://guest-house-fawn.vercel.app',
